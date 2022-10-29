ef96ff3c0650757c3f1d70f2e46183ceab57bb3b
Erick Schwartz <94484450+ecredeem@users.noreply.github.com>
2022-10-29 12:18:08 -0500

    Add files via upload

---
+++ LyondellbasellChallenge.cpp
@@ -0,0 +1,46 @@
+// chalenege prompt
+
+chemical team > polymers > terraforming mars 
+
+// Don't run out of resources:
+
+/*
+
+Autonomous devices should be able to come home
+
+drones or devices run out of power > BECOME IN TROUBLE
+
+------look at power
+
+------Collect data, regardless of no internet, record it locally
+
+------- HD Video will make device storage will run out of memory space
+
+Solutions: MAKE RECHARBLE STATIONS AND REDUCE MEMORY SPACE USAGE
+
+
+5 dead zones
+
+3 rechargble zones
+
+
+
+
+*/
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+  string AutoHome;
+  string drones;
+
+  return 0;
+}
+
+
+
